87d0e2a08d2a6541a7889588647334bab1923a2e
ІО-04 Бондаренко Юлія <98831174+Julia20032303@users.noreply.github.com>
2023-03-05 23:49:30 +0200

    Update Point.cs

--- src/Primitives3D/Point.cs
+++ src/Primitives3D/Point.cs
@@ -1,9 +1,33 @@
-﻿namespace Primitives3D;
+using System;
+
+namespace Primitives3D;
 
 public struct Point
 {
-    public Point(float x, float y, float z)
-    {
-        // todo
-    }
+        public float X { get; }
+        public float Y { get; }
+        public float Z { get; }
+
+        public Point(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Point operator +(Point point, Vector vector)
+        {
+            float x = point.X + vector.X;
+            float y = point.Y + vector.Y;
+            float z = point.Z + vector.Z;
+            return new Point(x, y, z);
+        }
+
+        public static Point operator -(Point point, Vector vector)
+        {
+            float x = point.X - vector.X;
+            float y = point.Y - vector.Y;
+            float z = point.Z - vector.Z;
+            return new Point(x, y, z);
+        }
 }
