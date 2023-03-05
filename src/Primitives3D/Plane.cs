dc729a8cf4ac7536a5e55ea2e77f9856820df4c6
Maksym <m.datsenko@pnn.in.ua>
2023-03-05 23:59:38 +0200

    Add plain definition.

---
+++ src/Primitives3D/Plane.cs
@@ -0,0 +1,13 @@
+﻿namespace Primitives3D;
+
+public struct Plane
+{
+    public Normal Normal { get; }
+    public Point Point { get; }
+
+    public Plane(Normal normal, Point point)
+    {
+        Normal = normal;
+        Point = point;
+    }
+}
