b67986f00172ea9303391f2ada6e5fc69d105b3c
Maksym <m.datsenko@pnn.in.ua>
2023-03-05 19:11:49 +0200

    Add vector math test class.

---
+++ src/PrimitivesTest/VectorMathTests.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimitivesTest;
+
+public sealed class VectorMathTests
+{
+    //todo: add tests for Vector.Add method.
+
+    //todo: add tests for Vector.Subtract method.
+
+    //todo: add tests for Vector.DotProduct method.
+
+    //todo: add tests for Vector.CrossProduct method.
+}
