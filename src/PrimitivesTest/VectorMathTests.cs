119a188b94b51c175e0f3735b10ab0efd92c9c78
ІО-04 Бондаренко Юлія <98831174+Julia20032303@users.noreply.github.com>
2023-03-06 00:21:10 +0200

    Update VectorMathTests.cs

--- src/PrimitivesTest/VectorMathTests.cs
+++ src/PrimitivesTest/VectorMathTests.cs
@@ -1,12 +1,64 @@
-﻿namespace PrimitivesTest;
+using Primitives3D;
+using NUnit.Framework;
 
-public sealed class VectorMathTests
+namespace PrimitivesTest;
 {
-    //todo: add tests for Vector.Add method.
+    public sealed class VectorTests
+    {
+        [Test]
+        public void Add_ShouldReturnCorrectVector()
+        {
+            // Arrange
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(4, 5, 6);
 
-    //todo: add tests for Vector.Subtract method.
+            // Act
+            var result = Vector.Add(v1, v2);
 
-    //todo: add tests for Vector.DotProduct method.
+            // Assert
+            Assert.AreEqual(new Vector(5, 7, 9), result);
+        }
 
-    //todo: add tests for Vector.CrossProduct method.
+        [Test]
+        public void Subtract_ShouldReturnCorrectVector()
+        {
+            // Arrange
+            var v1 = new Vector(4, 5, 6);
+            var v2 = new Vector(1, 2, 3);
+
+            // Act
+            var result = Vector.Subtract(v1, v2);
+
+            // Assert
+            Assert.AreEqual(new Vector(3, 3, 3), result);
+        }
+
+        [Test]
+        public void DotProduct_ShouldReturnCorrectValue()
+        {
+            // Arrange
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(4, 5, 6);
+
+            // Act
+            var result = Vector.DotProduct(v1, v2);
+
+            // Assert
+            Assert.AreEqual(32, result);
+        }
+
+        [Test]
+        public void CrossProduct_ShouldReturnCorrectVector()
+        {
+            // Arrange
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(4, 5, 6);
+
+            // Act
+            var result = Vector.CrossProduct(v1, v2);
+
+            // Assert
+            Assert.AreEqual(new Vector(-3, 6, -3), result);
+        }
+    }
 }
