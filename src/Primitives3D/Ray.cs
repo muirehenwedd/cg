7e0d891674761b978ba1d492450a75a9afd7d881
ІО-04 Бондаренко Юлія <98831174+Julia20032303@users.noreply.github.com>
2023-03-05 23:52:51 +0200

    Update Ray.cs

--- src/Primitives3D/Ray.cs
+++ src/Primitives3D/Ray.cs
@@ -1,9 +1,39 @@
-﻿namespace Primitives3D;
+namespace Primitives3D;
 
 public struct Ray
 {
-    public Ray(Point point, Vector vector)
+    public Point Origin;
+    public Vector Direction;
+
+    public Ray(Point origin, Vector direction)
     {
-        // todo
+        Origin = origin;
+        Direction = direction;
+    }
+
+    public Ray(Point p1, Point p2)
+    {
+        Origin = p1;
+        Direction = (p2 - p1).Normalize();
+    }
+
+    public Ray(Ray other)
+    {
+        Origin = other.Origin;
+        Direction = other.Direction;
+    }
+
+    public Point GetNearestPointTo(Point point)
+    {
+        Vector diff = point - Origin;
+        double projection = Vector.Dot(diff, Direction);
+        if (projection <= 0)
+        {
+            return Origin;
+        }
+        else
+        {
+            return Origin + Direction * projection;
+        }
     }
 }
