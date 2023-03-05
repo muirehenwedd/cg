6ce601d6c472c379fff064079d35685b667faf9f
ІО-04 Бондаренко Юлія <98831174+Julia20032303@users.noreply.github.com>
2023-03-05 22:45:59 +0200

    Update Vector.cs

--- src/Primitives3D/Vector.cs
+++ src/Primitives3D/Vector.cs
@@ -1,33 +1,51 @@
-﻿namespace Primitives3D;
+namespace Primitives3D;
 
 public struct Vector
 {
+    public float X { get; }
+    public float Y { get; }
+    public float Z { get; }
+
     public static Vector Add(Vector left, Vector right)
     {
-        // todo
-        throw new NotImplementedException();
+        float x = left.X + right.X;
+        float y = left.Y + right.Y;
+        float z = left.Z + right.Z;
+        
+        return new Vector(x, y, z);
     }
 
     public static Vector Subtract(Vector left, Vector right)
     {
-        // todo
-        throw new NotImplementedException();
+        float x = left.X - right.X;
+        float y = left.Y - right.Y;
+        float z = left.Z - right.Z;
+
+        return new Vector(x, y, z);
     }
 
     public static float DotProduct(Vector left, Vector right)
     {
-        // todo
-        throw new NotImplementedException();
+        float x = left.X * right.X;
+        float y = left.Y * right.Y;
+        float z = left.Z * right.Z;
+
+        return x + y + z;
     }
 
     public static float CrossProduct(Vector left, Vector right)
     {
-        // todo
-        throw new NotImplementedException();
+        float x = left.Y * right.Z - left.Z * right.Y;
+        float y = left.Z * right.X - left.X * right.Z;
+        float z = left.X * right.Y - left.Y * right.X;
+
+        return new Vector(x, y, z);
     }
 
     public Vector(float x, float y, float z)
     {
-        // todo
+        X = x;
+        Y = y;
+        Z = z;
     }
 }
