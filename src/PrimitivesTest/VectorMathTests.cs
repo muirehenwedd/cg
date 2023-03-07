463febb699275602a249ac3d1d877165cf2cf9f5
Maksym <m.datsenko@pnn.in.ua>
2023-03-07 11:44:39 +0200

    Fix VectorMathTests.

--- src/PrimitivesTest/VectorMathTests.cs
+++ src/PrimitivesTest/VectorMathTests.cs
@@ -1,64 +1,62 @@
 using Primitives3D;
-using NUnit.Framework;
 
 namespace PrimitivesTest;
+
+public sealed class VectorTests
 {
-    public sealed class VectorTests
+    [Fact]
+    public void Add_ShouldReturnCorrectVector()
     {
-        [Test]
-        public void Add_ShouldReturnCorrectVector()
-        {
-            // Arrange
-            var v1 = new Vector(1, 2, 3);
-            var v2 = new Vector(4, 5, 6);
+        // Arrange
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
 
-            // Act
-            var result = Vector.Add(v1, v2);
+        // Act
+        var result = Vector.Add(v1, v2);
 
-            // Assert
-            Assert.AreEqual(new Vector(5, 7, 9), result);
-        }
+        // Assert
+        Assert.Equal(new Vector(5, 7, 9), result);
+    }
 
-        [Test]
-        public void Subtract_ShouldReturnCorrectVector()
-        {
-            // Arrange
-            var v1 = new Vector(4, 5, 6);
-            var v2 = new Vector(1, 2, 3);
+    [Fact]
+    public void Subtract_ShouldReturnCorrectVector()
+    {
+        // Arrange
+        var v1 = new Vector(4, 5, 6);
+        var v2 = new Vector(1, 2, 3);
 
-            // Act
-            var result = Vector.Subtract(v1, v2);
+        // Act
+        var result = Vector.Subtract(v1, v2);
 
-            // Assert
-            Assert.AreEqual(new Vector(3, 3, 3), result);
-        }
+        // Assert
+        Assert.Equal(new Vector(3, 3, 3), result);
+    }
 
-        [Test]
-        public void DotProduct_ShouldReturnCorrectValue()
-        {
-            // Arrange
-            var v1 = new Vector(1, 2, 3);
-            var v2 = new Vector(4, 5, 6);
+    [Fact]
+    public void DotProduct_ShouldReturnCorrectValue()
+    {
+        // Arrange
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
 
-            // Act
-            var result = Vector.DotProduct(v1, v2);
+        // Act
+        var result = Vector.DotProduct(v1, v2);
 
-            // Assert
-            Assert.AreEqual(32, result);
-        }
+        // Assert
+        Assert.Equal(32, result);
+    }
 
-        [Test]
-        public void CrossProduct_ShouldReturnCorrectVector()
-        {
-            // Arrange
-            var v1 = new Vector(1, 2, 3);
-            var v2 = new Vector(4, 5, 6);
+    [Fact]
+    public void CrossProduct_ShouldReturnCorrectVector()
+    {
+        // Arrange
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
 
-            // Act
-            var result = Vector.CrossProduct(v1, v2);
+        // Act
+        var result = Vector.CrossProduct(v1, v2);
 
-            // Assert
-            Assert.AreEqual(new Vector(-3, 6, -3), result);
-        }
+        // Assert
+        Assert.Equal(new Vector(-3, 6, -3), result);
     }
 }
