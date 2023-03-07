365112f47404ad3d015c478204fe25ac497ac148
ІО-04 Бондаренко Юлія <98831174+Julia20032303@users.noreply.github.com>
2023-03-07 03:29:59 +0200

    Fix struct Vector; add normalize, multi operators

--- src/Primitives3D/Vector.cs
+++ src/Primitives3D/Vector.cs
@@ -41,11 +41,40 @@ public struct Vector
 
         return new Vector(x, y, z);
     }
+    public static Vector operator *(Vector v, float scalar)
+    {
+        var x = v.X * scalar;
+        var y = v.Y * scalar;
+        var z = v.Z * scalar;
 
+        return new Vector(x, y, z);
+    }
+    public static Vector operator *(float scalar, Vector vector)
+    {
+        return vector * scalar;
+    }
+    public static Vector Normalize(Vector vector)
+    {
+        float length = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y + vector.Z * vector.Z);
+
+        if (length == 0)
+        {
+            throw new DivideByZeroException("Cannot normalize a zero-length vector.");
+        }
+
+        return new Vector(vector.X / length, vector.Y / length, vector.Z / length);
+    }
+    public Vector(Point start, Point direction) : this()
+    {
+        X = direction.X - start.X;
+        Y = direction.Y - start.Y;
+        Z = direction.Z - start.Z;
+    }
     public Vector(float x, float y, float z)
     {
         X = x;
         Y = y;
         Z = z;
     }
+    
 }
