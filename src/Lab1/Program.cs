bf62ef2c25e8b4e2bf69e48a5514e52d3fb30add
Maksym <m.datsenko@pnn.in.ua>
2023-03-08 13:21:02 +0200

    Fix simple camera; remove old implementation.

--- src/Lab1/Program.cs
+++ src/Lab1/Program.cs
@@ -3,16 +3,22 @@ using Primitives3D;
 
 var scene = new SimpleScene();
 
-var lightSource = new SimpleLightSource(new Vector(-3, 0, 1));
+var lightSource = new SimpleLightSource(new Vector(-1, 0, 0));
 scene.SetLightSource(lightSource);
 
-var sphere1 = new Sphere(new Point(15, 0, 0), 10);
+var sphere1 = new Sphere(new Point(15, -5, -5), 3);
 scene.AddRayIntersectableObject(sphere1);
 
-//var sphere2 = new Sphere(new Point(15, 5, 10), 7);
-//scene.SetSphere(sphere2);
+var sphere2 = new Sphere(new Point(15, 5, -5), 3);
+scene.AddRayIntersectableObject(sphere2);
 
-var camera = new SimpleCamera() {PixelSize = 1};
+var sphere3 = new Sphere(new Point(15, 5, 5), 3);
+scene.AddRayIntersectableObject(sphere3);
+
+var sphere4 = new Sphere(new Point(15, -5, 5), 3);
+scene.AddRayIntersectableObject(sphere4);
+
+var camera = new SimpleCamera(5, 45, 80);
 scene.SetCamera(camera);
 
 ConsoleOutput.ShadesDisplay(scene.Render());
--- src/Lab1/SimpleCamera.cs
+++ src/Lab1/SimpleCamera.cs
@@ -5,20 +5,38 @@ namespace Lab1;
 
 public sealed class SimpleCamera : ICamera
 {
-    public required float PixelSize { get; init; }
+    private float _screenSize;
+    private float _screenDistance;
+    private int _pixelsOnOneSide;
+    private float _pixelSize;
+
+    public SimpleCamera(float screenDistance, float fieldOfViewDegrees, int pixelsOnOneSide)
+    {
+        _screenSize = MathF.Tan(fieldOfViewDegrees / 180 * MathF.PI) * screenDistance * 2;
+
+        _screenDistance = screenDistance;
+        _pixelsOnOneSide = pixelsOnOneSide;
+
+        _pixelSize = _screenSize / pixelsOnOneSide;
+    }
+
     public Point ViewPoint { get; } = new Point(0, 0, 0);
 
     public Point[,] GetCentersOfPixels()
     {
-        var halfPixelSize = PixelSize / 2;
+        var halfPixelSize = _pixelSize / 2;
+        var halfScreenSize = _screenSize / 2;
 
-        var pointsArray = new Point[20, 20];
+        var pointsArray = new Point[_pixelsOnOneSide, _pixelsOnOneSide];
 
-        for (var i = 0; i < 20; i++)
+        var startCoordinate = halfScreenSize - halfPixelSize;
+
+        for (var i = 0; i < _pixelsOnOneSide; i++)
         {
-            for (var j = 0; j < 20; j++)
+            for (var j = 0; j < _pixelsOnOneSide; j++)
             {
-                pointsArray[i, j] = new Point(5, 10 - j - halfPixelSize, 10 - i - halfPixelSize);
+                pointsArray[i, j] = new Point(_screenDistance, startCoordinate - _pixelSize * j,
+                    startCoordinate - _pixelSize * i);
             }
         }
 
