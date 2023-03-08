5ecfe877592128b998ce645fbbfcad5b2e1d74cc
Maksym <m.datsenko@pnn.in.ua>
2023-03-08 10:04:36 +0200

    Rename and move.

---
+++ src/Lab1/SimpleLightSource.cs
@@ -0,0 +1,14 @@
+﻿using Primitives3D;
+using Primitives3D.Abstractions;
+
+namespace Lab1;
+
+public struct SimpleLightSource : ILightSource
+{
+    public Vector Direction { get; }
+
+    public SimpleLightSource(Vector direction)
+    {
+        Direction = Vector.Normalize(direction);
+    }
+}
