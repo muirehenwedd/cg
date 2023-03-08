aec7ceda59c2de6b28874153b03f60e1347fddf1
Maksym <m.datsenko@pnn.in.ua>
2023-03-08 10:26:26 +0200

    Add simple implementation of ICamera, with actually hardcoded point positions.

---
+++ src/Lab1/SimpleCamera.cs
@@ -0,0 +1,27 @@
+﻿using Primitives3D;
+using Primitives3D.Abstractions;
+
+namespace Lab1;
+
+public sealed class SimpleCamera : ICamera
+{
+    public required float PixelSize { get; init; }
+    public Point ViewPoint { get; } = new Point(0, 0, 0);
+
+    public Point[,] GetCentersOfPixels()
+    {
+        var halfPixelSize = PixelSize / 2;
+
+        var pointsArray = new Point[20, 20];
+
+        for (var i = 0; i < 20; i++)
+        {
+            for (var j = 0; j < 20; j++)
+            {
+                pointsArray[i, j] = new Point(5, 10 - j - halfPixelSize, 10 - i - halfPixelSize);
+            }
+        }
+
+        return pointsArray;
+    }
+}
