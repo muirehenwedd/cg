f53f9cbf3e6ad136b843ebf7a1d81f2fd9bfd738
Maksym <m.datsenko@pnn.in.ua>
2023-04-12 15:19:35 +0300

    Add benchmark.

---
+++ src/Lab1/Benchie.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using Lab1.Output;
+using Primitives3D;
+
+namespace Lab1;
+
+[MemoryDiagnoser(false)]
+public class Benchie
+{
+    [Benchmark]
+    public float[,] RenderScene()
+    {
+        var scene = new SimpleScene();
+
+        var lightSource = new SimpleLightSource(new Vector(-1, 0, 0));
+        scene.SetLightSource(lightSource);
+
+        var sphere2 = new Sphere(new Point(15, 5, -5), 3);
+        scene.AddRayIntersectableObject(sphere2);
+
+        var sphere3 = new Sphere(new Point(15, 5, 5), 3);
+        scene.AddRayIntersectableObject(sphere3);
+
+        var disc1 = new Disc(new Plane(new Normal(new Point(15, -5, 5), new Vector(1, 0, 0))), 3);
+        scene.AddRayIntersectableObject(disc1);
+
+        var camera = new SimpleCamera(5, 45, 80);
+        scene.SetCamera(camera);
+
+        var output = new ConsoleShadesImageOutput();
+        return scene.Render();
+    }
+}
