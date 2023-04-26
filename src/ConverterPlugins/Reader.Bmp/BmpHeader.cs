1e1905459ad01775edc05c892f9c90f2d5a2300f
Maksym <m.datsenko@pnn.in.ua>
2023-04-26 22:41:08 +0300

    Add bmp reader.

---
+++ src/ConverterPlugins/Reader.Bmp/BmpHeader.cs
@@ -0,0 +1,42 @@
+﻿namespace Reader.Bmp;
+
+public struct BmpHeader
+{
+    public char B;
+    public char M;
+    public int FileSize;
+    public int Reserved;
+    public int HeaderSize;
+    public int InfoSize;
+    public int Width;
+    public int Height;
+    public short BiPlanes;
+    public short Bits;
+    public int BiCompression;
+    public int BiSizeImage;
+    public int BiXPelsPerMeter;
+    public int BiYPelsPerMeter;
+    public int BiClrUsed;
+    public int BiClrImportant;
+
+    public static BmpHeader ReadFromBinaryReader(BinaryReader reader) =>
+        new BmpHeader
+        {
+            B = reader.ReadChar(),
+            M = reader.ReadChar(),
+            FileSize = reader.ReadInt32(),
+            Reserved = reader.ReadInt32(),
+            HeaderSize = reader.ReadInt32(),
+            InfoSize = reader.ReadInt32(),
+            Width = reader.ReadInt32(),
+            Height = reader.ReadInt32(),
+            BiPlanes = reader.ReadInt16(),
+            Bits = reader.ReadInt16(),
+            BiCompression = reader.ReadInt32(),
+            BiSizeImage = reader.ReadInt32(),
+            BiXPelsPerMeter = reader.ReadInt32(),
+            BiYPelsPerMeter = reader.ReadInt32(),
+            BiClrUsed = reader.ReadInt32(),
+            BiClrImportant = reader.ReadInt32()
+        };
+}
---
+++ src/ConverterPlugins/Reader.Bmp/BmpReader.cs
@@ -0,0 +1,58 @@
+﻿using Converter;
+using Converter.Abstractions;
+
+namespace Reader.Bmp;
+
+public class BmpReader : IImageReader
+{
+    public bool CanRead(byte[] bytes)
+    {
+        using var reader = new BinaryReader(new MemoryStream(bytes));
+        var header = BmpHeader.ReadFromBinaryReader(reader);
+
+        if (header.B != 'B' || header.M != 'M')
+            return false;
+
+        if (header.Reserved != 0)
+            return false;
+
+        if (header.BiPlanes != 1)
+            return false;
+
+        if (header.Bits != 24)
+            return false;
+
+        return true;
+    }
+
+    public MediateImage Read(byte[] bytes)
+    {
+        using var memoryStream = new MemoryStream(bytes);
+        using var reader = new BinaryReader(memoryStream);
+        var header = BmpHeader.ReadFromBinaryReader(reader);
+
+        memoryStream.Seek(header.HeaderSize, SeekOrigin.Begin);
+
+        var mediateImage = new MediateImage(header.Height, header.Width);
+
+        var extraBytesPerRow = header.Width * 3 % 4 == 0 ? 0 : 4 - (header.Width * 3 % 4);
+        for (var i = header.Height - 1; i >= 0; i--)
+        {
+            for (var j = 0; j < header.Width; j++)
+            {
+                var b = reader.ReadByte();
+                var g = reader.ReadByte();
+                var r = reader.ReadByte();
+
+                mediateImage.Pixels[i, j] = new Pixel(r, g, b);
+            }
+
+            for (var k = 0; k < extraBytesPerRow; k++)
+            {
+                reader.ReadByte();
+            }
+        }
+
+        return mediateImage;
+    }
+}
