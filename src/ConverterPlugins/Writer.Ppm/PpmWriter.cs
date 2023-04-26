aabe84b4815ce32aa06ee6f9a4078ad065b1e39d
Maksym <m.datsenko@pnn.in.ua>
2023-04-26 17:53:02 +0300

    Add ppm writer.

---
+++ src/ConverterPlugins/Writer.Ppm/PpmWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Converter;
+using Converter.Abstractions;
+
+namespace Writer.Ppm;
+
+public sealed class PpmWriter : IImageWriter
+{
+    public string OutputFileExtension => "ppm";
+
+    public byte[] Write(MediateImage image)
+    {
+        var builder = new StringBuilder();
+
+        builder.AppendLine($"""
+        P3
+        {image.Width} {image.Height}
+        255
+        """);
+
+        foreach (var (r, g, b) in image.Pixels)
+        {
+            builder.AppendLine($"""
+            {r}
+            {g}
+            {b}
+            """);
+        }
+
+        return Encoding.ASCII.GetBytes(builder.ToString());
+    }
+}
