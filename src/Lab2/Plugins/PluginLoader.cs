8a0b1902028e7ed6583448abddce33bd47971eee
Maksym <m.datsenko@pnn.in.ua>
2023-04-27 14:07:53 +0300

    Add plugin loader and manager.

---
+++ src/Lab2/Plugins/PluginLoader.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using Converter.Abstractions;
+using Lab2.Plugins.Abstractions;
+
+namespace Lab2.Plugins;
+
+public sealed class PluginLoader : IPluginLoader
+{
+    private static readonly DirectoryInfo PluginsDirectory = new("Plugins");
+
+    public IImageWriter[] LoadWriters()
+    {
+        var writerFiles = PluginsDirectory.EnumerateFiles("Writer.*.dll");
+
+        return writerFiles
+            .Select(f =>
+            {
+                var assembly = Assembly.LoadFile(f.FullName);
+
+                var readerType = assembly
+                    .GetTypes()
+                    .FirstOrDefault(t => t.GetInterface(nameof(IImageWriter)) is not null);
+
+                if (readerType is null)
+                    return null;
+
+                return (IImageWriter) Activator.CreateInstance(readerType)!;
+            })
+            .Where(r => r is not null)
+            .ToArray()!;
+    }
+
+    public IImageReader[] LoadReaders()
+    {
+        var readerFiles = PluginsDirectory.EnumerateFiles("Reader.*.dll");
+
+        return readerFiles
+            .Select(f =>
+            {
+                var assembly = Assembly.LoadFile(f.FullName);
+
+                var readerType = assembly
+                    .GetTypes()
+                    .FirstOrDefault(t => t.GetInterface(nameof(IImageReader)) is not null);
+
+                if (readerType is null)
+                    return null;
+
+                return (IImageReader) Activator.CreateInstance(readerType)!;
+            })
+            .Where(r => r is not null)
+            .ToArray()!;
+    }
+}
---
+++ src/Lab2/Plugins/PluginManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using Converter;
+using Converter.Abstractions;
+using Lab2.Plugins.Abstractions;
+
+namespace Lab2.Plugins;
+
+public sealed class PluginManager : IPluginManager
+{
+    private readonly IImageWriter[] _imageWriters;
+    private readonly IImageReader[] _imageReaders;
+
+    public PluginManager(IPluginLoader pluginLoader)
+    {
+        _imageReaders = pluginLoader.LoadReaders();
+        _imageWriters = pluginLoader.LoadWriters();
+    }
+
+    public bool WriterPluginExists(string formatName)
+    {
+        return _imageWriters.Any(w =>
+            w.OutputFileExtension.Equals(formatName, StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    public bool TryReadImage(byte[] bytes, out MediateImage? mediateImage)
+    {
+        var reader = _imageReaders.FirstOrDefault(r => r!.CanRead(bytes));
+
+        if (reader is null)
+        {
+            mediateImage = default;
+            return false;
+        }
+
+        mediateImage = reader.Read(bytes);
+        return true;
+    }
+
+    public byte[] WriteImage(MediateImage mediateImage, string formatName)
+    {
+        var writer = _imageWriters.First(w =>
+            w.OutputFileExtension.Equals(formatName, StringComparison.InvariantCultureIgnoreCase));
+
+        return writer.Write(mediateImage);
+    }
+}
