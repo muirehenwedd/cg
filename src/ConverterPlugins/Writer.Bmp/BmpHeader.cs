8ae4e6c961b0426b850134aa599ea662e53749bc
Maksym <m.datsenko@pnn.in.ua>
2023-04-27 14:05:44 +0300

    Add bmp writer.

---
+++ src/ConverterPlugins/Writer.Bmp/BmpHeader.cs
@@ -0,0 +1,31 @@
+﻿namespace Writer.Bmp;
+
+public sealed class BmpHeader
+{
+    public int FileSize { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public static readonly int HeaderSize = 54;
+    public static readonly int InfoSize = 40;
+
+    public void WriteToBinaryWriter(BinaryWriter writer)
+    {
+        writer.Write('B');
+        writer.Write('M');
+        writer.Write(FileSize);
+        writer.Write(0);
+        writer.Write(HeaderSize);
+        writer.Write(InfoSize);
+        writer.Write(Width);
+        writer.Write(Height);
+        writer.Write((short)1);
+        writer.Write((short)24);
+        writer.Write(0);
+        writer.Write(0);
+        writer.Write(0);
+        writer.Write(0);
+        writer.Write(0);
+        writer.Write(0);
+    }
+}
---
+++ src/ConverterPlugins/Writer.Bmp/BmpWriter.cs
@@ -0,0 +1,46 @@
+﻿using Converter;
+using Converter.Abstractions;
+
+namespace Writer.Bmp;
+
+public class BmpWriter : IImageWriter
+{
+    public string OutputFileExtension => "bmp";
+
+    public byte[] Write(MediateImage image)
+    {
+        var extraBytesPerRow = image.Width * 3 % 4 == 0 ? 0 : 4 - (image.Width * 3 % 4);
+        var fileSize = BmpHeader.HeaderSize + BmpHeader.InfoSize + (image.Width * 3 + extraBytesPerRow) * image.Height;
+
+        using var memoryStream = new MemoryStream();
+        using var writer = new BinaryWriter(memoryStream);
+
+        var header = new BmpHeader
+        {
+            FileSize = fileSize,
+            Width = image.Width,
+            Height = image.Height
+        };
+
+        header.WriteToBinaryWriter(writer);
+
+        for (var i = image.Height - 1; i >= 0; i--)
+        {
+            for (var j = 0; j < image.Width; j++)
+            {
+                var pixel = image.Pixels[i, j];
+
+                writer.Write(pixel.B);
+                writer.Write(pixel.G);
+                writer.Write(pixel.R);
+            }
+
+            for (var k = 0; k < extraBytesPerRow; k++)
+            {
+                writer.Write(byte.MinValue);
+            }
+        }
+
+        return memoryStream.ToArray();
+    }
+}
