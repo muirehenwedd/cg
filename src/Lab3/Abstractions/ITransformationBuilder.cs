092a53ca9d3450fbd958dcd954419fc8d4f6892f
Maksym <m.datsenko@pnn.in.ua>
2023-05-07 13:11:00 +0300

    Add transformation classes.

---
+++ src/Lab3/Abstractions/ITransformationBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab3.Abstractions;
+
+public interface ITransformationBuilder
+{
+    ITransformationBuilder Translate(float deltaX, float deltaY, float deltaZ);
+    ITransformationBuilder Scale(float xScaleFactor, float yScaleFactor, float zScaleFactor);
+    ITransformationBuilder RotateAroundX(float angleInDegrees);
+    ITransformationBuilder RotateAroundY(float angleInDegrees);
+    ITransformationBuilder RotateAroundZ(float angleInDegrees);
+
+    Transformation BuildTransformation();
+    ITransformationBuilder Reset();
+}
---
+++ src/Lab3/Transformation.cs
@@ -0,0 +1,50 @@
+﻿using Primitives3D;
+
+namespace Lab3;
+
+public sealed class Transformation
+{
+    private readonly Matrix _transformationMatrix;
+
+    public Transformation(Matrix transformationMatrix)
+    {
+        _transformationMatrix = transformationMatrix;
+    }
+
+    public Triangle Apply(Triangle input) => _transformationMatrix.Transform(input);
+    public Point Apply(Point input) => _transformationMatrix.Transform(input);
+    public Vector Apply(Vector input) => _transformationMatrix.Transform(input);
+    public Normal Apply(Normal input) => _transformationMatrix.Transform(input);
+
+    public void Apply(Triangle[] input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            input[i] = Apply(input[i]);
+        }
+    }
+    
+    public void Apply(Point[] input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            input[i] = Apply(input[i]);
+        }
+    }
+    
+    public void Apply(Vector[] input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            input[i] = Apply(input[i]);
+        }
+    }
+    
+    public void Apply(Normal[] input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            input[i] = Apply(input[i]);
+        }
+    }
+}
